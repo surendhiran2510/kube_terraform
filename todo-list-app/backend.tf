71b6a5969f328080025c8654ec649fb85d2c1ada
Surendhiran <sureymadhu@gmail.com>
2024-01-04 12:27:50 +0530

    Update backend.tf

--- todo-list-app/backend.tf
+++ todo-list-app/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "learning-terraform"
+    bucket = "terraform-on-aws-for-ec2-cd"
     key    = "backend/Todo-app-eks.tfstate"
     region = "us-east-1"
-    dynamodb_table = "dynamoDB-state-locking"
+    dynamodb_table = "iacdevops-dev-tfstate"
   }
 }
